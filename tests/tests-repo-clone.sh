f8fed520be18751f474bd1693edb009a71a37867
Andreas Holmsten <andreas.holmsten@elastx.se>
2018-09-06 10:13:03 +0200

    Initial Pike support

--- tests/tests-repo-clone.sh
+++ tests/tests-repo-clone.sh
@@ -96,7 +96,7 @@ elif [[ ! -d tests/common ]]; then
     # Otherwise we're clearly not in zuul or using a previously setup
     # repo in some way, so just clone it from upstream.
     else
-        git clone -b stable/queens \
+        git clone -b stable/pike \
             https://git.openstack.org/openstack/openstack-ansible-tests \
             ${WORKING_DIR}/tests/common
     fi
